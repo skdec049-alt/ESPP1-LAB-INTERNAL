9ab1469791874ce395e4133be6750ac8164e9ebd
skdec049-alt <skdec049@gmail.com>
2026-02-19 18:15:15 +0530

    app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def main():
     # Restrict input between 1 and 60
     roll_no = st.number_input(
         "Enter Roll Number (50-110)", 
-        min_value=50, 
-        max_value=110, 
+        min_value=01, 
+        max_value=02, 
         step=1
     )
 
